a1fca5acc60e3540421f9b64219416856aadab58
iamdevops07 <nikunjvasava26@gmail.com>
2022-10-13 00:13:26 +0530

    adding aws and k8s challenge

---
+++ aws-terraform-challenge-1/main.tf
@@ -0,0 +1,25 @@
+resource "aws_key_pair" "citadel-key" {
+  key_name   = "citadel"
+  public_key = file("${path.module}/.ssh/ec2-connect-key.pub")
+}
+
+provider "aws" {
+  profile    = "default"
+  region     = "us-east-1"
+}
+
+resource "aws_instance" "citadel" {
+  ami           = var.ami
+  instance_type = var.instance_type
+  user_data     = file("install-nginx.sh")
+  provisioner "local-exec" {
+    command = "echo ${self.public_dns} >> /root/citadel_public_dns.txt"
+  }
+}
+
+resource "aws_eip" "EC@_eip" {
+  instance = aws_instance.citadel.id
+  vpc      = true
+}
+
+
